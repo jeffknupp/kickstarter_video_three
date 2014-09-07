cd9608e3f46b978e87fe13388e3c24e82039cbf4
Jeffery Knupp <jknupp@jefferys-mbp.home>
2014-09-07 15:08:28 -0400

    complete

--- proxy.py
+++ proxy.py
@@ -3,7 +3,7 @@ other things."""
 
 # Standard libary imports
 import socket
-import thread
+import threading
 import select
 
 __version__ = '0.1.0 Draft 1'
@@ -12,12 +12,11 @@ VERSION = 'Python Proxy/{}'.format(__version__)
 HTTP_VERSION = 'HTTP/1.1'
 
 
-class ConnectionHandler(object):
+class Proxy(object):
     """Handles connections between the HTTP client and HTTP server."""
-    def __init__(self, connection, _, timeout):
+    def __init__(self, connection):
         self.client = connection
         self.client_buffer = ''
-        self.timeout = timeout
         self.target = None
         method, path, protocol = self.get_base_header()
         if method == 'CONNECT':
@@ -28,19 +27,14 @@ class ConnectionHandler(object):
     def get_base_header(self):
         """Return a tuple of (method, path, protocol) from the recieved
         message."""
-        while 1:
+        while '\n' not in self.client_buffer:
             self.client_buffer += self.client.recv(BUFFER_LENGTH)
-            end = self.client_buffer.find('\n')
-            if end != -1:
-                break
-        print '{}'.format(self.client_buffer[:end])
-        data = (self.client_buffer[:end+1]).split()
-        self.client_buffer = self.client_buffer[end+1:]
-        return data
+        (data, _, self.client_buffer) = self.client_buffer.partition('\n')
+        return data.split()
 
     def method_connect(self, path):
         """Handle HTTP CONNECT messages."""
-        self._connect_target(path)
+        self._connect_to_target(path)
         self.client.send('{http_version} 200 Connection established\n'
                          'Proxy-agent: {version}\n\n'.format(
                              http_version=HTTP_VERSION,
@@ -50,11 +44,10 @@ class ConnectionHandler(object):
 
     def method_others(self, method, path, protocol):
         """Handle all non-HTTP CONNECT messages."""
-        path = path[7:]
-        i = path.find('/')
-        host = path[:i]
-        path = path[i:]
-        self._connect_target(host)
+        path = path[len('http://'):]
+        host, _, path = path.partition('/')
+        path = '/{}'.format(path)
+        self._connect_to_target(host)
         self.target.send('{method} {path} {protocol}\n{client_buffer}'.format(
             method=method,
             path=path,
@@ -63,58 +56,52 @@ class ConnectionHandler(object):
         self.client_buffer = ''
         self._read_write()
 
-    def _connect_target(self, host):
+    def _connect_to_target(self, host):
         """Create a connection to the HTTP server specified by *host*."""
-        i = host.find(':')
-        if i != -1:
-            port = int(host[i+1:])
-            host = host[:i]
-        else:
-            port = 80
-        (soc_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
-        self.target = socket.socket(soc_family)
+        port = 80
+        if ':' in host:
+            host, _, port = host.partition(':')
+        (socket_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
+        self.target = socket.socket(socket_family)
         self.target.connect(address)
 
     def _read_write(self):
         """Read data from client connection and forward to server
         connection."""
-        time_out_max = self.timeout/3
-        socs = [self.client, self.target]
-        count = 0
-        while 1:
-            count += 1
-            (recv, _, error) = select.select(socs, [], socs, 3)
-            if error:
-                break
-            if recv:
-                for in_ in recv:
-                    data = in_.recv(BUFFER_LENGTH)
-                    if in_ is self.client:
-                        out = self.target
-                    else:
-                        out = self.client
-                    if data:
-                        out.send(data)
-                        count = 0
-            if count == time_out_max:
-                break
-        self.client.close()
-        self.target.close()
+        sockets = [self.client, self.target]
+        try:
+            while 1:
+                (receive, _, error) = select.select(sockets, [], sockets, 10)
+                if error:
+                    break
+                elif receive:
+                    for source in receive:
+                        data = source.recv(BUFFER_LENGTH)
+                        if not data:
+                            return
+                        if source is self.client:
+                            destination = self.target
+                        else:
+                            destination = self.client
+                        destination.sendall(data)
+        finally:
+            self.client.close()
+            self.target.close()
 
 
-def start_server(host='localhost', port=8080, ipv_6=False, timeout=60,
-                 handler=ConnectionHandler):
+def start_server():
     """Start the HTTP proxy server."""
-    if ipv_6:
-        soc_type = socket.AF_INET6
-    else:
-        soc_type = socket.AF_INET
-    soc = socket.socket(soc_type)
-    soc.bind((host, port))
+    host = 'localhost'
+    port = 8080
+    listener = socket.socket(socket.AF_INET)
+    listener.bind((host, port))
     print 'Serving on {0}:{1}.'.format(host, port)
-    soc.listen(0)
+    listener.listen(0)
     while 1:
-        thread.start_new_thread(handler, soc.accept()+(timeout,))
+        connection, address = listener.accept()
+        print 'Got connection from {}'.format(address)
+        threading.Thread(
+            target=Proxy, args=(connection, )).run()
 
 if __name__ == '__main__':
     start_server()
